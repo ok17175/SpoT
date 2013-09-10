687070dfb3667655f11f3df0a2cf433389eebd17
ok17175 <ok17175@gmail.com>
2013-09-10 22:01:33 +0800

    Initial Commit

---
+++ SpoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpoT
+//
+//  Created by 李深龙 on 13-9-10.
+//  Copyright (c) 2013年 李深龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpoT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpoT
+//
+//  Created by 李深龙 on 13-9-10.
+//  Copyright (c) 2013年 李深龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
